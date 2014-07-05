413ce588d8d2ae2af175b0780d39151cb346b1c7
Richard <rail.rich@gmail.com>
2014-07-05 19:44:49 -0400

    Bah tser le endlessWorld truc

--- Assets/Scripts/EndlessWorld/EndlessWorld.cs
+++ Assets/Scripts/EndlessWorld/EndlessWorld.cs
@@ -10,16 +10,21 @@ public class EndlessWorld : MonoBehaviour {
 	public int worldCreationDistance=2;
 	public int worldCreationWidthToCreate=1;
 	public int statNbBuilding = 0;
-	public int buildingInARow = 0;
+	public float remainingWithInARow = 0;
 	public GameObject world;
 	public GameObject buildings;
+	public GameObject roads;
 
 	public GameObject assetBuilding;
 	public GameObject assetRoad;
 
+	public RandomBuildingConfig config1;
+	public Sprite[] roadSprites;
+
 	void Start () {
 		world = GameObjectFactory.createGameObject ("World");
 		buildings = GameObjectFactory.createGameObject ("Buildings",world.transform);
+		roads = GameObjectFactory.createGameObject ("Roads",world.transform);
 
 		random = new Random ();
 		Random.seed = seed;
@@ -35,11 +40,21 @@ public class EndlessWorld : MonoBehaviour {
 
 	void Update () {
 		if (needToCreate ()) {
-			int widthRemaining = worldCreationWidthToCreate;
+			float widthRemaining = worldCreationWidthToCreate;
 			while(widthRemaining > 0){
-				//if(buildingInARow )
-				int widthCreated = generateOneBuilding();
+				if(remainingWithInARow <= 0){
+					int skipedWidth = Random.Range(2,6);
+					for(int i = 0; i < skipedWidth; i++){
+						Sprite roadSprite = roadSprites [Random.Range (0, roadSprites.Length)];
+						createSpriteGameObject(roads.transform, 2, roadSprite, new Vector2(createdWorldDimension.x+i - (1f/2f),-3f));
+					}
+					createdWorldDimension = new Vector2 (createdWorldDimension.x + skipedWidth, createdWorldDimension.y);
+					remainingWithInARow = Random.Range(10,20);
+
+				}
+				float widthCreated = generateOneBuilding();
 				widthRemaining -= widthCreated;
+				remainingWithInARow -= widthCreated;
 			}
 		}
 	}
@@ -48,19 +63,50 @@ public class EndlessWorld : MonoBehaviour {
 		return this.transform.position.x + worldCreationDistance > createdWorldDimension.x;
 	}
 
-	private int generateOneBuilding(){
+	private float generateOneBuilding(){
 		statNbBuilding++;
-		int buildingWidth = Random.Range(1,3);
-		GameObject newBuilding = GameObjectFactory.createGameObject ("BuildingPart" + statNbBuilding, this.buildings.transform);
+		RandomBuildingConfig c = this.config1;
+		int buildingWidth = Random.Range(c.min.width,c.max.width);
+		int buildingHeight = Random.Range(c.min.height,c.max.height);
+		if (buildingWidth <= 0) buildingWidth = 1;
+		if (buildingHeight <= 0) buildingHeight = 1;
+
+		//float offset = Random.Range (0, 0.2f);
+		Sprite buildingSprite = c.getRandomBuildingSprite ();
+		Sprite trimSprite = c.getRandomTrimSprite ();
+		Sprite roadSprite = roadSprites [Random.Range (0, roadSprites.Length)];
+
+		GameObject newBuilding = GameObjectFactory.createGameObject ("Building " + statNbBuilding, this.buildings.transform);
 		for (int x = 0; x < buildingWidth; x++) {
-			for (int y = 0; y < Random.Range(1,5); y++) {
-				GameObject buildingPart = GameObjectFactory.createCopyGameObject (this.assetBuilding, "Part" + statNbBuilding, newBuilding);
-				buildingPart.transform.Translate(new Vector2(x,y));
-			}		
+			for (int y = 0; y < buildingHeight; y++) {
+				createSpriteGameObject(newBuilding.transform, 0, buildingSprite, new Vector2(x*2,y*2));
+			}
+			float roadX = createdWorldDimension.x + x*2 - (1f/2f);
+			createSpriteGameObject(roads.transform, 2, roadSprite, new Vector2(roadX,-3f));
+			createSpriteGameObject(roads.transform, 2, roadSprite, new Vector2(roadX+1f,-3f));
+			float trimX = x*2 - (1f/2f);
+			createSpriteGameObject(newBuilding.transform, 1, trimSprite, new Vector2(trimX,-0.75f));
+			createSpriteGameObject(newBuilding.transform, 1, trimSprite, new Vector2(trimX,buildingHeight*2 - 1.25f));
+			createSpriteGameObject(newBuilding.transform, 1, trimSprite, new Vector2(trimX+1f,-0.75f));
+			createSpriteGameObject(newBuilding.transform, 1, trimSprite, new Vector2(trimX+1f,buildingHeight*2 - 1.25f));
 		}
-		Debug.Log (buildingWidth * 2);
+
+
+
+
+		float wholeWidth = buildingWidth * 2;
 		newBuilding.transform.Translate (createdWorldDimension);
-		createdWorldDimension = new Vector2 (createdWorldDimension.x + buildingWidth * 2, createdWorldDimension.y);
-		return buildingWidth * 2;
+		createdWorldDimension = new Vector2 (createdWorldDimension.x + wholeWidth, createdWorldDimension.y);
+		return wholeWidth;
+	}
+
+	private void createSpriteGameObject(Transform parent, int sortingOrder, Sprite sprite, Vector2 position){
+		GameObject buildingPart = GameObjectFactory.createGameObject("Part", parent);
+		buildingPart.AddComponent<SpriteRenderer>();
+		SpriteRenderer sr = buildingPart.GetComponent<SpriteRenderer> ();
+		sr.sprite = sprite;
+		sr.sortingOrder = sortingOrder;
+
+		buildingPart.transform.Translate(position);
 	}
 }
---
+++ Assets/Scripts/EndlessWorld/Randomiser/RandomBuildingConfig.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class RandomBuildingConfig {
+
+	public Dimension min;
+	public Dimension max;
+
+	public Sprite[] buildingSprites;
+	public Sprite[] trimSprites;
+
+	public Sprite getRandomBuildingSprite(){
+		return buildingSprites[Random.Range(0,buildingSprites.Length)];
+	}
+
+	public Sprite getRandomTrimSprite(){
+		return trimSprites[Random.Range(0,trimSprites.Length)];
+	}
+}
