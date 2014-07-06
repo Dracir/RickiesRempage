0bf5a37f62860e7e14e88b246a4ea12992e61593
Richard <rail.rich@gmail.com>
2014-07-06 14:14:28 -0400

    Destroyable garbage!

--- Assets/Scripts/EndlessWorld/EndlessWorld.cs
+++ Assets/Scripts/EndlessWorld/EndlessWorld.cs
@@ -11,7 +11,6 @@ public class EndlessWorld : MonoBehaviour {
 	public Random random;
 	public int worldCreationDistance=2;
 	public int worldCreationWidthToCreate=1;
-	public int statNbBuilding = 0;
 	public float remainingWithInARow = 0;
 
 	void Start () {
@@ -46,7 +45,6 @@ public class EndlessWorld : MonoBehaviour {
 	}
 
 	private float generateOneBuilding(){
-		statNbBuilding++;
 		RandomBuildingConfig c = this.creator.config1;
 		int buildingWidth = Random.Range(c.min.width,c.max.width);
 		int buildingHeight = Random.Range(c.min.height,c.max.height);
@@ -57,25 +55,9 @@ public class EndlessWorld : MonoBehaviour {
 		Sprite buildingSprite = c.getRandomBuildingSprite ();
 		Sprite trimSprite = c.getRandomTrimSprite ();
 
-
-		GameObject newBuilding = GameObjectFactory.createGameObject ("Building " + statNbBuilding, this.creator.buildings.transform);
-		Transform parent = newBuilding.transform;
-		for (int x = 0; x < buildingWidth; x++) {
-			for (int y = 0; y < buildingHeight; y++) {
-				creator.createBuildingPart(parent, buildingSprite, new Vector2(x*2,y*2));
-			}
-			float roadX = createdWorldDimension.x + x*2 - (1f/2f);
-			creator.createRoad(new Vector2(roadX,0));
-			creator.createRoad(new Vector2(roadX + 1f,0));
-			float trimX = x*2 - (1f/2f);
-			creator.createTrim(parent, trimSprite, new Vector2(trimX,0));
-			creator.createTrim(parent, trimSprite, new Vector2(trimX,buildingHeight*2 - 0.75f));
-			creator.createTrim(parent, trimSprite, new Vector2(trimX+1,0));
-			creator.createTrim(parent, trimSprite, new Vector2(trimX+1,buildingHeight*2 - 0.75f));
-		}
+		creator.createBuilding (createdWorldDimension,buildingSprite, trimSprite, buildingWidth, buildingHeight);
 
 		float wholeWidth = buildingWidth * 2;
-		newBuilding.transform.Translate (createdWorldDimension);
 		createdWorldDimension = new Vector2 (createdWorldDimension.x + wholeWidth, createdWorldDimension.y);
 		return wholeWidth;
 	}
---
+++ Assets/Scripts/World/Props.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Props : MonoBehaviour {
+
+	private SpriteRenderer sr;
+	public string spriteName;
+
+	public Sprite[] sprites;
+
+	void Start () {
+
+	}
+
+	public void setName(string spriteName){
+		this.spriteName = spriteName;
+		sr = this.GetComponent<SpriteRenderer> ();
+
+		sprites = Resources.LoadAll<Sprite> (spriteName);
+		
+		sr.sprite = sprites[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void HalfHealth () {
+		Debug.Log ("YOO 1/2");
+		sr.sprite = sprites[2];
+	}
+	
+	void Destroyed() {
+		Debug.Log ("YOO");
+		sr.sprite = sprites[3];
+	}
+
+	void Hit(){
+		Debug.Log ("aaa");
+		sr.sprite = sprites[1];
+	}
+}
